3295e9ba01ad42f52d3ecf3cbaa6de8229264d3c
badnaim <batnyaam.tumurbat@gmail.com>
2022-11-29 00:02:01 +0800

    js

---
+++ javascript/exercise1/datatypes.js
@@ -0,0 +1,16 @@
+// 5. Datatypes.js нэртэй файл үүсгэх, typeof ашиглан өөр, өөр, төрлийн утгуудыг шалгах, number, string, boolean, undefined,  null гэх мэт 
+
+let number = "15";
+console.log(typeof number);
+
+let realNumber = 15;
+console.log(typeof realNumber);
+
+let isTrue = true;
+console.log(typeof isTrue);
+
+let myVar = null;
+console.log(typeof myVar);
+
+var myVar1;
+console.log(typeof myVar1);
