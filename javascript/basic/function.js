91c2e3fe1082eb64d957e509ac37421813ac6dec
Morning <morning@admins-iMac.local>
2022-11-28 12:14:41 +0800

    bug fixed

---
+++ javascript/basic/function.js
@@ -0,0 +1,82 @@
+// string-ug oguulber "..."
+let name = "batnyam";
+
+// number-too
+let number1 = 2;
+let number2 = 3;
+let number3 = number1 + number2;
+
+// boolean-true false
+let isTrue = true;
+
+// number1 = 100;
+
+let num1 = 4,
+  num2 = 5;
+
+// var-iig orchin uyd ashiglahaa bolison ghde variable
+var name1 = "batnyam";
+
+// constant - togtmol
+const PI = 3.14;
+
+console.log(number3);
+console.log(name);
+console.log("hello world");
+
+// this is my code. semi-slash=;
+// double quotation mark=""
+// reserved words in js to search !!!!!!!!!!!!!!!!!!!
+// assignment operator - suulgaj ogoh
+
+let numberMy1 = "12";
+let numberMy2 = "14";
+
+let num5 = 15;
+let num6 = 16;
+
+console.log(numberMy1 + numberMy2);
+console.log(num5 + num6);
+
+// ----------
+
+let abs = '"24"';
+console.log(abs);
+
+// let inputNumber = prompt("enter a number");
+let inputNumber = 2022;
+let birthYear = 2022 - inputNumber;
+// let variable1 = prompt();
+
+console.log("-----------------------");
+
+let a = 8;
+let b = 9;
+console.log("" + a + b);
+console.log(a + b + "");
+console.log(a + "" + b);
+console.log(a + " " + b);
+
+let nuum1 = 685;
+let nuum2 = (nuum1 - (nuum1 % 10)) / 10;
+let nuum3 = (nuum2 - (nuum2 % 10)) / 10;
+
+console.log(nuum3);
+
+console.log("-----------------------");
+
+let number = "15";
+console.log(typeof number);
+let realNumber = parseInt(number);
+let realNumber1 = parseFloat(number);
+console.log(typeof realNumber);
+console.log(realNumber);
+console.log(realNumber1);
+
+// parse-horwuuleh int-
+// dom-html,css bom-browser
+
+let nam1 = parseFloat(prompt("enter first number"));
+let nam2 = parseFloat(prompt("enter second number"));
+alert("2 toonii niilber n: " + (nam1 + nam2));
+alert("ehnii toog 2 dahi toond huwaahad uldegdel n : " + (num1 % num2));
---
+++ javascript/exercise1/function.js
@@ -0,0 +1,43 @@
+// exercise1-----------------js basic--------------
+
+console.log("-------------");
+
+let myNumber1 = 3;
+let myNumber2 = 2;
+let nemeh = "Subtraction of given 2 numbers is ";
+let hasah = "Addition of given 2 numbers is ";
+
+console.log(nemeh + (myNumber1 + myNumber2));
+console.log(hasah + (myNumber1 - myNumber2));
+console.log(myNumber1 * myNumber2);
+console.log(myNumber1 / myNumber2);
+console.log(myNumber1 % myNumber2);
+
+// exercise2-----------------js basic---------------
+
+//1 comments can make code readable
+
+//2 javascript lesson beign
+
+/*3 comments 
+can 
+make 
+code 
+readable*/
+
+//4 null
+let myVar = null;
+
+alert(myVar);
+
+// undefined
+var myVar1;
+
+alert(myVar);
+
+// utga onoogoogui huwisagch zarlah
+let hello;
+
+// utga onooson huwisagch zarlah
+
+let world = "mindless";
