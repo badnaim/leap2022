480e4ecd14828e2e339c3030728bf5871f420e56
badnaim <batnyaam.tumurbat@gmail.com>
2022-11-30 16:47:53 +0800

    js easy dasgal hiilee

--- js-day2/exercise2/easy/function.js
+++ js-day2/exercise2/easy/function.js
@@ -16,6 +16,8 @@ if (0 <= n && n < 60) {
   console.log("Buruu dun oruulsan bn.");
 }
 
+console.log("-----------------");
+
 // 2. сарын дугаар буюу n - 1<=n=>12-д хувьсагчид байх утгын дагуу тухайн сарын өдрийн тоог хэвлэнэ.хэрэв 2 дугаар сар бол 28 or 29 гэж хэвлэнэ
 
 let jan = 31,
@@ -61,17 +63,19 @@ if (m === 1) {
   console.log("1-12 sariig songono uu");
 }
 
+console.log("-----------------");
+
 // 3. Өгөгдсөн эерэг тоо нь 3-ын үржвэр эсвэл 7-ын үржвэр эсэхийг шалгана уу
 
-let w = 3,
+let w = 5,
   firstW = w % 3,
   secW = w % 7;
 
-if (firstW === 0 && secW === 0) {
-  console.log("3 ba 7-iin urjwer mun");
-} else {
-  console.log("3 ba 7-iin urjwer bish");
-}
+// if (firstW === 0 && secW === 0) {
+//   console.log("3 ba 7-iin urjwer mun");
+// } else {
+//   console.log("3 ba 7-iin urjwer bish");
+// }
 
 if (firstW === 0 || secW === 0) {
   if (firstW === 0 && secW !== 0) {
@@ -84,6 +88,8 @@ if (firstW === 0 || secW === 0) {
 } else {
   console.log("3 ba 7-iin urjwer bish");
 }
+
+console.log("-----------------");
 
 // 4. n  хувьсагчын утгын дагуу эерэг эсвэл сөрөгийг олж хэвлэнэ.
 // negative
@@ -102,6 +108,8 @@ if (ki) {
   }
 }
 
+console.log("-----------------");
+
 // 5. weight, height гэсэн 2 хувьсагчид утга оруулахад тухайн хэмжээнээс хамааран биеийн жингийн индексийг тооцож тухайн индекс-д харгалзах утгыг хэвлэнэ.
 // Жингийн дутагдалтай
 // Эрүүл
@@ -114,18 +122,49 @@ let weight = 60,
 
 if (index < 18.5) {
   console.log("jingiin dutagdaltai as fuk");
-} else if (18.5 <= index && index > 25) {
+} else if (18.5 <= index && index < 25) {
   console.log("Eruul bna");
-} else if (25 <= index && index > 30) {
+} else if (25 <= index && index < 30) {
   console.log("targan bna");
-} else if (30 <= index && index > 40) {
+} else if (30 <= index && index < 40) {
   console.log("het ih targan bn");
 } else {
   console.log("error");
 }
-console.log(index);
-// 6. Өгөгдсөн тооны тэгш эсвэл сондгой болохыг хэвлэж харуулна уу
+
+
+console.log("-----------------");
+
+// 6. Өгөгдсөн тооны тэгш эсвэл сондгой болохыг хэвлэж харуулна уу  odd-sondgoi even-tegsh
+
+let number0 =3,
+oddOrEven = number0 % 2;
+
+if (oddOrEven === 0) {
+    console.log("tegsh too");
+  }else {
+    console.log("sondgoi too");
+}
+
+console.log("-----------------");
+
 // 7. Өгөгдсөн тооны эерэг тоо эсвэл сөрөг тоо болохыг шалгаад хэвлэж харуулна уу
+
+let number2 = -4;
+
+if (Math.sign(number2) === 1) {
+  console.log("eyreg too");
+}else if (Math.sign(number2) === -1) {
+  console.log("sorog too");
+}else if (Math.sign(number2) === 0) {
+  console.log("eyreg too"); 
+}else {
+  console.log("too oruulna uu");
+}
+
+
+console.log("-----------------");
+
 // 8. Нас тооцоолох бодлого: yearOfBirth гэсэн хувьсагчид гараас төрсөн онийг ав. Тухайн оноос тухайн хүний насыг нь бодож гаргаад үр дүнгээ old гэсэн хувьсагчид оноо. Насанд нь харгалзах утгыг хэвлэ.
 // 0 <= old <= 1 үед "Infant"
 // 1 < old <= 3 үед "Toddler"
@@ -134,3 +173,24 @@ console.log(index);
 // 12 <old <= 18 үед "Teen"
 // 18 < old <=21 үед "Young adult"
 // 21 < old үед "Adult"
+
+let yearOfBirth = 2002,
+    old = 2022 - 2002;
+
+if (0 <= old && old <= 1) {
+  console.log("Infant");
+}else if (1 < old && old <= 3) {
+  console.log("Toddler");
+}else if (3 < old && old <= 5) {
+  console.log("Preschool");
+}else if (5 < old && old <= 12) {
+  console.log("Gradeschooler");
+}else if (12 < old && old <= 18) {
+  console.log("Teen");
+}else if (18 < old && old <= 21) {
+  console.log("Young adult");
+}else if (21 < old) {
+  console.log("Adult");
+}else {
+  console.log("NaN");
+}
