d094933dcf8295f88f3df801487e910bf3fe2b8d
badnaim <batnyaam.tumurbat@gmail.com>
2022-12-04 16:17:15 +0800

    escape sequences

---
+++ js-day3/exercise3/escape-sequences/app.js
@@ -0,0 +1,5 @@
+// Hello I am "Name Here".
+// I am 'Age here' years old.
+//     This is the new line with tab and some \backslashes\.
+
+console.log(`Hello I am "Name Here". \nI am 'Age here' years old. \n\tThis is new line with tab and some \\backslashes\\.`);
