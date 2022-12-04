cc44725de8a09a184cde9afed24f61a078296f41
badnaim <batnyaam.tumurbat@gmail.com>
2022-12-04 16:37:06 +0800

    numbers of sum

---
+++ js-day3/exercise3/numbers-sum/app.js
@@ -0,0 +1,12 @@
+// 3 оронтой тоо ( n ) ороход тухайн 3 оронтой тооны цифрүүдийн нийлбэрийг ол.
+
+let n = 123;
+let x = n.toString(),
+    a = x.charAt(0),
+    b = x.charAt(1),
+    c = x.charAt(2);
+let a1 = Math.floor(a),
+    b1 = Math.floor(b),
+    c1 = Math.floor(c);
+    
+console.log(a1 + b1 + c1);
