5156358f4e36a75933873e442eca708f40dc54b4
badnaim <batnyaam.tumurbat@gmail.com>
2022-12-04 23:27:41 +0800

    orgil bagshaas bodoltiig asuuh

---
+++ js-day4/exercise4/number-digits/app.js
@@ -0,0 +1,24 @@
+// 1. Prompt - оор 1 тоо авна.
+
+// let number = window.prompt("1 тоо оруулна уу: ");
+// let number = "565656";
+
+// 2. Prompt - оор авсан тоог хэдэн оронтойг хэвлэж гарга.
+// example: input = 1234567; digit: = 7;
+
+// console.log(number.length);
+
+
+// -----------------------------------
+
+
+// let a = prompt("Ta hussen neg toogoo oruulna uu");
+let a = 898989;
+let count = 0;
+
+
+while(a !== 0){
+    count  = count + 1;
+    a = Math.floor(a / 10);
+}
+console.log(count);
