fd65b2845cc56fdb921c43dccdfef066b0e0fbf6
badnaim <batnyaam.tumurbat@gmail.com>
2022-12-04 14:40:02 +0800

    simple assignment asuuh oguulber oilgohgui bna

---
+++ js-day3/exercise3/simple-assignment/app.js
@@ -0,0 +1,13 @@
+// 1. 0-ээс 100 хүртэл санамсаргүй тоог үүсгэнэ үү.
+
+const min = 0;
+const max = 100;
+const a = Math.floor(Math.random() * (max - min + 1)) + min;
+console.log(a);
+
+// 2. 50-ээс 255 хүртэлх санамсаргүй тоог үүсгэнэ үү
+
+const min1 = 55;
+const max1 = 255;
+const b = Math.floor(Math.random() * (max1 - min1 + 1)) + min1;
+console.log(b);
