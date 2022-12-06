422f33bac49b4576b903dd707aaab443dd9bfd2f
badnaim <batnyaam.tumurbat@gmail.com>
2022-12-06 08:16:49 +0800

    n

---
+++ js-day5/exercise5/urwuu-too/app.js
@@ -0,0 +1,13 @@
+// 12345 -> 54321 n хувьсагчийн утгыг тоог урвуул.
+
+let number = 12345;
+let a = console.log(number.length);
+let b = console.log(a);
+let space = "";
+
+for (i = 0; i <= 4; i++) {
+  number.length;
+  space = s;
+  a = a - 1;
+}
+console.log(space);
