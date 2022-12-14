7d06d6557c182eb97037415a1e48b093d722c8ea
badnaim <batnyaam.tumurbat@gmail.com>
2022-12-14 16:01:13 +0800

    delguuriin tailan hiilee

--- js-day9/delguuriin-tailan/app.js
+++ js-day9/delguuriin-tailan/app.js
@@ -151,7 +151,7 @@ function percent(data) {
     sum2 = data[j].unitPrice * data[j].amount;
     totalPrice1 += sum2;
   }
-  // console.log(totalPrice1);
+
   console.log(
     "Хамгийн их зарагдсан 3 барааны боруулалтын үнийн дүнгийн нийт боруулалтанд эзлэх хувь:",
     Math.round((sum1 / totalPrice) * 100) + "%"
@@ -159,10 +159,68 @@ function percent(data) {
 }
 percent(data);
 
+console.log("-------------------------------------");
+
 // 8. Хамгийн их зарагдсан /боруулалтын тоо хэмжээгээр/ 5 барааны тоо хэмжээ нийт барааны тоо хэмжээнд эзлэх хувь харуулах.
-let sum3 = 8,
+
+let sum3 = 0,
   totalAmount8 = 0;
 data.sort((a, b) => b.amount - a.amount);
-function percentAmount(data) {}
+function percentAmount(data) {
+  for (i = 0; i < data.length; i++) {
+    totalAmount8 += data[i].amount;
+  }
+  for(j = 0; j < 3; j++) {
+    sum3 += data[j].amount;
+  }
+  console.log(
+    "Хамгийн их зарагдсан 3 барааны боруулалтын үнийн дүнгийн нийт боруулалтанд эзлэх хувь:",
+    Math.round((sum3 / totalAmount8) * 100) + "%"
+  );
+}percentAmount(data);
+
+console.log("-------------------------------------");
+
 // 9. Хамгийн бага зарагдсан /боруулалтын үнийн дүнгээр/ 5 барааны боруулалтын үнийн дүнгийн нийт боруулалтанд эзлэх хувь харуулах.
+
+let sum4 = 0,
+  totalPrice9 = 0;
+
+data.sort((a, b) => a.totalPrice - b.totalPrice);
+function percentYoungTotalPrise(data) {
+  for (i = 0; i < 3; i++) {
+    sum4 += data[i].unitPrice * data[i].amount;
+  }
+  for (j = 0; j < data.length; j++) {
+    sum2 = data[j].unitPrice * data[j].amount;
+    totalPrice9 += sum2;
+  }
+
+  console.log(
+    "Хамгийн бага зарагдсан 3 барааны тоо хэмжээ нийт борлуулсан барааны тоонд эзлэх хувь:",
+    Math.round((sum4 / totalPrice9) * 100) + "%"
+  );
+}
+percentYoungTotalPrise(data);
+
+console.log("-------------------------------------");
+
 // 10. Хамгийн бага зарагдсан /боруулалтын тоо хэмжээгээр/ 5 барааны тоо хэмжээ нийт барааны тоо хэмжээнд эзлэх хувь харуулах.
+
+let sum5 = 0,
+  totalAmount0 = 0;
+data.sort((a, b) => a.amount - b.amount);
+function percentYoungAmount(data) {
+  for (i = 0; i < data.length; i++) {
+    totalAmount0 += data[i].amount;
+  }
+  for(j = 0; j < 3; j++) {
+    sum5 += data[j].amount;
+  }
+  console.log(
+    "Хамгийн бага зарагдсан 3 барааны тоо хэмжээ нийт борлуулсан барааны тоонд эзлэх хувь:",
+    Math.round((sum5 / totalAmount0) * 100) + "%"
+  );
+}percentYoungAmount(data);
+
+console.log("-------------------------------------");
