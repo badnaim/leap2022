1627953e9f9600f6b32e7808e5fec7e8383cbc21
Morning <morning@admins-iMac.local>
2022-12-14 12:48:27 +0800

    12/14 wednesday

--- js-day9/delguuriin-tailan/app.js
+++ js-day9/delguuriin-tailan/app.js
@@ -50,20 +50,24 @@ const data = [
     totalPrice: 210000,
     casherId: 1,
     date: "2022-11-01",
-  }
+  },
 ];
 
 // Дараах даалгаварыг хийж гүйцэтгэнэ үү.
 // 1. Нийт борлуулалтын дүнг тооцоолох.
 
-  let sum = 0,
-      totalPrice = 0;
+console.log("-------------------------------------");
 
-  for (let i = 0; i < data.length; i++) {
-    sum = data[i].unitPrice * data[i].amount;
-    totalPrice += sum;
-  }
+let sum = 0,
+  totalPrice = 0;
+
+for (let i = 0; i < data.length; i++) {
+  sum = data[i].unitPrice * data[i].amount;
+  totalPrice += sum;
+}
 console.log("Нийт борлуулалтын дүн: " + totalPrice + " төгрөг");
+
+console.log("-------------------------------------");
 
 // 2. Нийт борлуулагдсан барааны тоог тооцоолох.
 
@@ -74,23 +78,21 @@ for (i = 0; i < data.length; i++) {
 }
 console.log("Нийт борлуулсан барааны тоо: " + totalAmount + " ширхэг");
 
-// 3. Хамгийн их зарагдсан 5 барааны жагсаалт /боруулалтын үнийн дүнгээр/ харуулах.
+console.log("------------------------------------");
 
-let sum1 = 0;
-let max1 = data[0].totalPrice;
-let max2 = data[0].totalPrice;
-let max3 = data[0].totalPrice;
-const sorted = data.sort((a, b) => a - b);
+// 3. Хамгийн их зарагдсан 5 барааны жагсаалт /борлуулалтын үнийн дүнгээр/ харуулах.
 
-for (i = 0; i < data.length; i++) {
-  if (data[i].totalPrice > max1) {
-    max1 = data[i].totalPrice;
-  }if (data[i].totalPrice > max2 && max2 !== max1) {
-    max2 = data[i].totalPrice
-  }if (data[i].totalPrice > max3 && max3 !== max2  && max3 !== max1) {
-    max3 = data[i].totalPrice
-  }
-}console.log(max1, max2, max3);
+data.sort((a, b) => b.totalPrice - a.totalPrice);
+console.log("Хамгийн их зарагдсан 5 бараа бор/үнэээр:");
+for (i = 0; i < 5; i++) {
+  console.log(
+    `Number ${i + 1} бараа: `,
+    data[i].productName,
+    data[i].totalPrice
+  );
+}
+
+// -------------------------------
 
 // function maximum () {
 //   let array = [5, 4, 2, 15, 10, 1];
@@ -102,10 +104,65 @@ for (i = 0; i < data.length; i++) {
 //   }
 // }
 
+console.log("-------------------------------------");
+
 // 4. Хамгийн их зарагдсан 5 барааны жагсаалт /боруулалтын тоо хэмжээгэр/ харуулах.
+
+data.sort((a, b) => b.amount - a.amount);
+console.log("Хамгийн их зарагдсан 5 бараа тоо хэмжээгээр:");
+for (i = 0; i < 5; i++) {
+  console.log(`Number ${i + 1} бараа: `, data[i].productName, data[i].amount);
+}
+
+console.log("-------------------------------------");
+
 // 5. Хамгийн бага зарагдсан 5 барааны жагсаалт /боруулалтын үнийн дүнгээр/ харуулах.
+
+data.sort((a, b) => a.totalPrice - b.totalPrice);
+console.log("Хамгийн бага зарагдсан 5 бараа бор/үнээр:");
+for (i = 0; i < 5; i++) {
+  console.log(data[i].productName, data[i].totalPrice);
+}
+
+console.log("-------------------------------------");
+
 // 6. Хамгийн бага зарагдсан 5 барааны жагсаалт /боруулалтын тоо хэмжээгээр/ харуулах.
-// 7. Хамгийн их зарагдсан /боруулалтын үнийн дүнгээр/ 10 барааны боруулалтын үнийн дүнгийн нийт боруулалтанд эзлэх хувь харуулах.
+
+data.sort((a, b) => a.amount - b.amount);
+console.log("Хамгийн бага зарагдсан 5 бараа тоо хэмжээгээр:");
+for (i = 0; i < 5; i++) {
+  console.log(data[i].productName, data[i].amount);
+}
+
+console.log("-------------------------------------");
+
+// 7. Хамгийн их зарагдсан /борлуулалтын үнийн дүнгээр/ 10 барааны боруулалтын үнийн дүнгийн нийт боруулалтанд эзлэх хувь харуулах.
+
+let sum2 = 0,
+  totalPrice1 = 0;
+
+data.sort((a, b) => b.totalPrice - a.totalPrice);
+function percent(data) {
+  let sum1 = 0;
+  for (i = 0; i < 3; i++) {
+    sum1 += data[i].totalPrice;
+  }
+  for (j = 0; j < data.length; j++) {
+    sum2 = data[j].unitPrice * data[j].amount;
+    totalPrice1 += sum2;
+  }
+  // console.log(totalPrice1);
+  console.log(
+    "Хамгийн их зарагдсан 3 барааны боруулалтын үнийн дүнгийн нийт боруулалтанд эзлэх хувь:",
+    Math.round((sum1 / totalPrice) * 100) + "%"
+  );
+}
+percent(data);
+
 // 8. Хамгийн их зарагдсан /боруулалтын тоо хэмжээгээр/ 5 барааны тоо хэмжээ нийт барааны тоо хэмжээнд эзлэх хувь харуулах.
+let sum3 = 8,
+  totalAmount8 = 0;
+data.sort((a, b) => b.amount - a.amount);
+function percentAmount(data) {}
 // 9. Хамгийн бага зарагдсан /боруулалтын үнийн дүнгээр/ 5 барааны боруулалтын үнийн дүнгийн нийт боруулалтанд эзлэх хувь харуулах.
 // 10. Хамгийн бага зарагдсан /боруулалтын тоо хэмжээгээр/ 5 барааны тоо хэмжээ нийт барааны тоо хэмжээнд эзлэх хувь харуулах.
