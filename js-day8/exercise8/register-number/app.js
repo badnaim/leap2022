098f0f44459f1f471854d74702b214259e4b8a12
badnaim <batnyaam.tumurbat@gmail.com>
2022-12-18 13:04:09 +0800

    register hiilee

--- js-day8/exercise8/register-number/app.js
+++ js-day8/exercise8/register-number/app.js
@@ -1,63 +1,204 @@
 // Дурын String авахад тухайн string нь зөв РД мөн эсэхийг шалгадаг isCorrectReg(register) функц бич.
+let mglAlphabet = [
+  "А",
+  "Б",
+  "В",
+  "Г",
+  "Д",
+  "Е",
+  "Ж",
+  "З",
+  "И",
+  "Й",
+  "К",
+  "Л",
+  "М",
+  "Н",
+  "О",
+  "П",
+  "Р",
+  "С",
+  "Т",
+  "Ф",
+  "Х",
+  "У",
+  "Ц",
+];
 
 function isCorrectReg(register) {
-  if (isNaN(register.charAt(0)) == true && isNaN(register.charAt(1)) == true) {
+  if (
+    mglAlphabet.includes(register.charAt(0)) == true &&
+    mglAlphabet.includes(register.charAt(1)) == true
+  ) {
     for (i = 2; i < 10; i++) {
       if (isNaN(register.charAt(i)) == false && register.length == 10) {
-        notNumber = "RD yes";
+        Number = "Регистэрийн дугаар зөв";
       } else {
-        notNumber = "RD no";
+        Number = "Регистэрийн дугаар биш";
         break;
       }
     }
-    console.log(notNumber);
+    console.log(Number);
   } else {
-    console.log("RD no");
+    console.log("Регистэрийн дугаар биш");
   }
 }
-isCorrectReg("МЗ02221711");
-
-// ----------------------
 
 // function isCorrectReg(reg) {
+//   let alphA = reg[0];
+//   let alphB = reg[1];
 //   if (
-//     alphabet.includes(reg[0]) &&
-//     alphabet.includes(reg[1]) &&
+//     mglAlphabet.includes(alphA) &&
+//     mglAlphabet.includes(alphB) &&
 //     Number.isInteger(Number(reg.slice(2, 10))) &&
-//     reg.length == 10
+//     reg.lenth == 10
 //   ) {
-//     console.log(reg);
+//     console.log("yes");
 //   } else {
-//     console.log("RD no");
+//     console.log("no");
 //   }
 // }
-// isCorrectReg("МЗ87878787");
+// isCorrectReg("МЗ02221711");
+
+console.log("-------------------------------------");
 
 // Хэрэв зөв бол тухайн РД аас хүйс авдаг getGender(register) функц бич / Хэрэв буруу РД байвал буруу байна гэж хэвлэнэ. /
 
+function getGender(register) {
+  if (
+    mglAlphabet.includes(register.charAt(0)) == true &&
+    mglAlphabet.includes(register.charAt(1)) == true
+  ) {
+    for (i = 2; i < 10; i++) {
+      if (isNaN(register.charAt(i)) == false && register.length == 10) {
+        if (register.charAt(8) % 2 == 0) {
+          Number = "Хүйс: эмэгтэй";
+        } else {
+          Number = "Хүйс: эрэгтэй";
+        }
+      } else {
+        Number = "Регистэрийн дугаар биш";
+        break;
+      }
+    }
+    console.log(Number);
+  } else {
+    console.log("Регистэрийн дугаар биш");
+  }
+}
+
+// function getGender(register, input) {
+
+// if (register.charAt(8) % 2 == 0) {
+//     if (input.includes("Эмэгтэй")) {
+//       console.log("Регистэрийн дугаар хүйстэй таарч байна");
+//     }else {
+//       console.log("РД хүйстэй таарахгүй байна");
+//     }
+//   }else {
+//     if (input.includes("Эрэгтэй")) {
+//       console.log("Регистэрийн дугаар хүйстэй таарч байна");
+//     }else {
+//       console.log("РД хүйстэй таарахгүй байна");
+//     }
+//   }
+
+// }
+// getGender("МЗ02721901", "Эмэгтэй");
+
+console.log("-------------------------------------");
+
 // РД аас төрсөн өдөр авдаг getBirthDay(register) функц бич / Хэрэв буруу РД байвал буруу байна гэж хэвлэнэ. /
-// РД аас төрсөн аймгийг олдог getBirthRegion(register)  функц бич / Хэрэв буруу РД байвал буруу байна гэж хэвлэнэ. /
+
+function getBirthDay(register) {
+  if (
+    mglAlphabet.includes(register.charAt(0)) == true &&
+    mglAlphabet.includes(register.charAt(1)) == true
+  ) {
+    for (i = 2; i < 10; i++) {
+      if (isNaN(register.charAt(i)) == false && register.length == 10) {
+        if (register.charAt(4) == 2 || register.charAt(4) == 3) {
+          Number = `Төрсөн он: ${
+            20 + register.charAt(2) + register.charAt(3)
+          } \nТөрсөн сар: ${0 + register.charAt(5)} \nТөрсөн өдөр: ${
+            register.charAt(6) + register.charAt(7)
+          }`;
+        } else if (register.charAt(4) == 0) {
+          Number = `Төрсөн он: ${
+            19 + register.charAt(2) + register.charAt(3)
+          } \nТөрсөн сар: ${0 + register.charAt(5)} \nТөрсөн өдөр: ${
+            register.charAt(6) + register.charAt(7)
+          }`;
+        } else if (register.charAt(4) == 1) {
+          Number = `Төрсөн он: ${
+            20 + register.charAt(2) + register.charAt(3)
+          } \nТөрсөн сар: ${
+            register.charAt(4) + register.charAt(5)
+          } \nТөрсөн өдөр: ${register.charAt(6) + register.charAt(7)}`;
+        } else {
+          Number = "Регистэрийн дугаар биш";
+          break;
+        }
+      } else {
+        Number = "Регистэрийн дугаар биш";
+        break;
+      }
+    }
+    console.log(Number);
+  } else {
+    console.log("Регистэрийн дугаар биш");
+  }
+}
+
+console.log("-------------------------------------");
+// РД аас төрсөн аймгийг олдог getBirthRegion(register)  функц бич / Хэрэв буруу РД байвал буруу байна гэж хэвлэнэ.
+
+let province = [
+  "Архангай",
+  "Баян-Өлгий",
+  "Баянхонгор",
+  "Булган",
+  "Говь-Алтай",
+  "Дорноговь",
+  "Дорнод",
+  "Дундговь",
+  "Завхан",
+  "Өвөрхангай",
+  "Өмнөговь",
+  "Сүхбаатар",
+  "Сэлэнгэ",
+  "Төв",
+  "Увс",
+  "Ховд",
+  "Хөвсгөл",
+  "Хэнтий",
+  "Дархан-Уул",
+  "Орхон",
+  "Говьсүмбэр",
+  "Улаанбаатар",
+  "Улаанбаатар",
+];
+
+function getBirthRegion(register) {
+  let indexKey;
+  let firstLetter = register[0];
+
+  if (mglAlphabet.includes(firstLetter)) {
+    indexKey = mglAlphabet.indexOf(firstLetter);
+    console.log(`Харьяа бүс нутаг: ${province[indexKey]}`);
+  } else {
+    console.log("Регистэрийн дугаар биш");
+  }
+}
+
+console.log("-------------------------------------");
+
 // Дээрх бүх утгыг хэвлэдэг getRegisterInfo(register) функц бичнэ үү.
-// А = Архангай
-// Б = Баян-Өлгий
-// В = Баянхонгор
-// Г = Булган
-// Д = Говь-Алтай
-// Е = Дорноговь
-// Ж = Дорнод
-// З = Дундговь
-// И = Завхан
-// Й = Өвөрхангай
-// К = Өмнөговь
-// Л = Сүхбаатар
-// М = Сэлэнгэ
-// Н = Төв
-// О = Увс
-// П = Ховд
-// Р = Хөвсгөл
-// С = Хэнтий
-// Т = Дархан-Уул
-// Ф = Орхон
-// Х = Говьсүмбэр
-// У = Улаанбаатар
-// Ц = Улаанбаатар
+
+function getRegisterInfo(registerID) {
+  isCorrectReg(registerID);
+  getGender(registerID);
+  getBirthDay(registerID);
+  getBirthRegion(registerID);
+}
+getRegisterInfo("МЗ02221711");
