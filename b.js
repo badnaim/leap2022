85d252ca4bb1fcdb6288ac3747ebea39fad70ee3
Morning <morning@admins-iMac.local>
2022-12-21 12:56:20 +0800

    new

---
+++ b.js
@@ -0,0 +1,12 @@
+document.getElementById("myBttn").onclick = () => {
+  let pass = document.getElementById("myPassword").value;
+  if (pass == "aqua") {
+    document.getElementById("warn").innerText = "CORRECT";
+    document.getElementById("warn").style.color = "green";
+    document.getElementById("button1").style.display = "block";
+  } else {
+    document.getElementById("warn").innerText =
+      "YOU ARE THE DUMBEST GUY EVER! BUT DO NOT QUIT:3";
+    document.getElementById("warn").style.color = "red";
+  }
+};
